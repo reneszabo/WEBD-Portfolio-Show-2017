023d826f5a7906ec1aa222cf38a9102e59349a67
Alison Hall <alisonh0101@gmail.com>
2017-03-08 15:12:49 -0500

    Add Firebase into the main page. Get the data from Firebase and iterate through to print out every person's name.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,39 @@
-(function() {
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyANKO5LqddgASdkSG2WzcPXLPQ41AkewPo",
+	authDomain: "webd-portfolio-show-2017.firebaseapp.com",
+	databaseURL: "https://webd-portfolio-show-2017.firebaseio.com",
+	storageBucket: "webd-portfolio-show-2017.appspot.com",
+	messagingSenderId: "888183054325"
+};
+firebase.initializeApp(config);
+var data;
 
-	// Initialize Firebase
-	var config = {
-		apiKey: "AIzaSyAnEDbmWbdr72O2IeCY1Qicj3c4LLkL9cU",
-		authDomain: "code-assist.firebaseapp.com",
-		databaseURL: "https://code-assist.firebaseio.com",
-		storageBucket: "code-assist.appspot.com",
-		messagingSenderId: "953126498792"
-	};
-	firebase.initializeApp(config);
-	
-}());
+function getData() {
+	firebase.database().ref('/users/').on('value', function(snapshot) {
+		data = snapshot.val();
+		var $people = $('#people');
+		var textHtml = '<ul>';
+
+		if(data !== null) {
+			for (var item in data) {
+				console.log(data[item]);
+				var firstName = data[item].firstName;
+				var lastName = data[item].lastName;
+				textHtml += '<li>' + firstName + ' ' + lastName + '</li>';
+			}
+		}
+
+		textHtml += '</ul>';
+
+		$people.html(textHtml);
+	});
+}
+
+function initApp() {
+	getData();
+}
+
+window.onload = function() {
+	initApp();
+};
